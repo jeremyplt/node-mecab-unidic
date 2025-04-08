2e005147c24871e90497649d915bc75bca2b92a9
Jeremy <89654634+jeremyplt@users.noreply.github.com>
2025-04-08 21:55:09 +0200

    increase build number

--- test/index.mjs
+++ test/index.mjs
@@ -11,13 +11,13 @@ import {
 const text = "「今度は美術館に行ってみようかな」とピエールは笑顔で言います"
 
 Promise.all([analyze(text), tokenize(text), wakati(text)]).then((results) => {
-	// for (const result of results) {
-	// 	console.dir(result, {depth: null});
-	// }
+	for (const result of results) {
+		console.dir(result, {depth: null});
+	}
 });
 
 const syncs = [analyzeSync, tokenizeSync, wakatiSync];
 
 for (const sync of syncs) {
-	// console.dir(sync(text), {depth: null});
+	console.dir(sync(text), {depth: null});
 }
